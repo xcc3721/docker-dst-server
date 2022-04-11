90b35e9af6ae9fe62c48d47bab1265001da44ff9
xichenchen <xichenchen@58.com>
2022-04-11 18:40:49 +0800

    add files

---
+++ dst_default_config/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,22 @@
+return {
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=1, kong=0, nillots=0, rendiao=1, zbdiao=true },
+    enabled=true 
+  } 
+}
